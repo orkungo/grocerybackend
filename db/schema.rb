ec4422e041ad18a662fbbaba7232d4f4a67b5028
orkungo <orkungo@yahoo.com>
2013-05-06 05:16:29 -0700

    expiration date added to products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130328143410) do
+ActiveRecord::Schema.define(:version => 20130506121026) do
 
   create_table "products", :force => true do |t|
     t.string   "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(:version => 20130328143410) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.integer  "shelf_id"
+    t.date     "expiration_date"
   end
 
   create_table "shelves", :force => true do |t|
